a2e4f287118aca72dc7037845e2d9e10f06bde84
krup-benzatine <krup@benzatine.com>
2026-01-06 16:41:51 +0530

    Initial commit

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 export const DBconnection = async () => {
-  const uri = "mongodb+srv://Krup:Krup%40%40%40krup@krupcluster.qpf0eyq.mongodb.net/?retryWrites=true&w=majority";
-
+  const uri = process.env.DB_URI;
+  if (!uri) {
+    console.error("DB_URI is not defined");
+    return;
+  }
   try {
     await mongoose.connect(uri);
     console.log("DB chal raha he");
